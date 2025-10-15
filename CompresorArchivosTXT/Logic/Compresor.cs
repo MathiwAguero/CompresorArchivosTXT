bbd8da76d075e0459472fcc10a07bd8dd6169b46
MathiwAguero <mathiw.aguero.hernandez@est.una.ac.cr>
2025-10-14 20:56:07 -0600

    Implementacion del compresor

--- CompresorArchivosTXT/Logic/Compresor.cs
+++ CompresorArchivosTXT/Logic/Compresor.cs
@@ -1,4 +1,6 @@
-﻿namespace CompresorArchivosTXT.Logic;
+﻿using System.Text;
+
+namespace CompresorArchivosTXT.Logic;
 
 public class Compresor
 {
@@ -20,13 +22,56 @@ public class Compresor
               writer.Write(bytesComprimidos.Length);
         }
     }
+    public (Dictionary<char, string>, Dictionary<char, int>) LeerArchivoComprimido(string rutaEntrada)
+    {
+        using (FileStream fs = new FileStream(rutaEntrada, FileMode.Open))
+        using (BinaryReader reader = new BinaryReader(fs))
+        {
+            int numSimbolos = reader.ReadInt32();
+            int numBitsComprimidos = reader.ReadInt32();
+            Dictionary<char, string> codigos = new Dictionary<char, string>();
+            Dictionary<char, int> frecuencias = new Dictionary<char, int>();
+            for (int i = 0; i < numSimbolos; i++)
+            {
+                char simbolo = reader.ReadChar();
+                int frecuencia = reader.ReadInt32();
+                byte longitudCodigo = reader.ReadByte();
+                char[] codigoChars = reader.ReadChars(longitudCodigo);
+                string codigo = new string(codigoChars);
+                codigos[simbolo] = codigo;
+                frecuencias[simbolo] = frecuencia;
+            }
+            byte[] bytesComprimidos = reader.ReadBytes((int)(fs.Length - fs.Position));
+            string bitsComprimidos = ConvertirBitsABites(bytesComprimidos, numBitsComprimidos);
+            return (codigos, frecuencias);
+        }
+    }
 
     private byte[] ConvertirBitsABytes(string bitsComprimidos)
     {
+        int numBytes = (bitsComprimidos.Length + 7) / 8;
         byte[] bytesComprimidos = new byte[bitsComprimidos.Length/8];
-        
-        
-        
+        for (int i = 0; i < bitsComprimidos.Length; i += 8)
+        {
+            if (bitsComprimidos[i] == '1')
+            {
+                int byteIndex = i / 8;
+                int bitIndex = 7 - (i % 8);
+                bytesComprimidos[byteIndex] |= (byte)(1 << bitIndex);
+            }
+        }
         return bytesComprimidos;
+    }
+    private string ConvertirBitsABites(byte[] bytesComprimidos, int numBitsComprimidos)
+    {
+        StringBuilder bitsComprimidos = new StringBuilder();
+        foreach (byte b in bytesComprimidos)
+        {
+            for(int i=7;i>=0;i--)
+            {
+                bitsComprimidos.Append((b & (1 << i)) != 0 ? '1' : '0');
+            }
+        }
+        return bitsComprimidos.ToString();
     }
 }
